4a3d9f5758734664edc0697eef15677378c18738
Roque Alarcon <roquealarcon363@gmail.com>
2020-10-03 18:13:25 -0500

    :heavy_plus_sign: Adding a Sass Stylesheet

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Redirect } from 'react-router-dom';
 import * as ROUTES from 'constants/routes';
 import RouteWithLayout from './RouteWithLayout';
 import { Main as MainLayout } from 'layouts';
@@ -7,21 +7,23 @@ import { Home as HomeView, NotFound as NotFoundView } from 'views';
 
 const Routes = () => {
   return (
-    <Switch>
-      <RouteWithLayout
-        component={HomeView}
-        exact
-        layout={MainLayout}
-        path={ROUTES.HOME}
-      />
-      <RouteWithLayout
-        component={NotFoundView}
-        exact
-        layout={MainLayout}
-        path={ROUTES.NOT_FOUND}
-      />
-      <Redirect from="*" to={ROUTES.NOT_FOUND} />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <RouteWithLayout
+          component={HomeView}
+          exact
+          layout={MainLayout}
+          path={ROUTES.HOME}
+        />
+        <RouteWithLayout
+          component={NotFoundView}
+          exact
+          layout={MainLayout}
+          path={ROUTES.NOT_FOUND}
+        />
+        <Redirect from="*" to={ROUTES.NOT_FOUND} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
